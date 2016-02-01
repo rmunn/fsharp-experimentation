ee8eaa61ea7b85cb60e62d74c74519d811bda228
Robin Munn <robin_munn@sil.org>
2016-02-01 11:26:56 +0700

    Basic example from http://suave.io
    Run Script.fsx to download dependencies (Suave and Paket) and start server.
    GET or POST http://localhost:8083/hello or http://localhost:8083/goodbye
    to see server in operation.

---
+++ Todo/Library1.fs
@@ -0,0 +1,18 @@
+﻿namespace Todo
+open Suave
+open Suave.Filters
+open Suave.Operators
+open Suave.Successful
+
+type Class1() = 
+    member this.X = "F#"
+
+module Todo =
+    let app =
+        choose
+            [ GET >=> choose
+                [ path "/hello" >=> OK "Hello GET"
+                  path "/goodbye" >=> OK "Goodbye GET" ]
+              POST >=> choose
+                [ path "/hello" >=> OK "Hello POST"
+                  path "/goodbye" >=> OK "Goodbye POST" ] ]
---
+++ Todo/Script.fsx
@@ -0,0 +1,38 @@
+﻿// Learn more about F# at http://fsharp.org. See the 'F# Tutorial' project
+// for more guidance on F# programming.
+
+// Define your library scripting code here
+
+// Step 0. Boilerplate to get the paket.exe tool
+ 
+open System
+open System.IO
+ 
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
+ 
+if not (File.Exists "paket.exe") then
+    let url = "https://github.com/fsprojects/Paket/releases/download/2.48.1/paket.exe"
+    use wc = new Net.WebClient()
+    let tmp = Path.GetTempFileName()
+    wc.DownloadFile(url, tmp)
+    File.Move(tmp,Path.GetFileName url);;
+ 
+// Step 1. Resolve and install the packages
+ 
+#r "paket.exe"
+ 
+Paket.Dependencies.Install """
+source https://nuget.org/api/v2
+nuget Suave
+""";;
+ 
+// Step 2. Use the packages
+ 
+#r "packages/Suave/lib/net40/Suave.dll"
+
+open Suave // always open suave
+
+#load "Library1.fs"
+open Todo
+
+startWebServer defaultConfig Todo.app // TODO: Use code from Library1.fs instead
