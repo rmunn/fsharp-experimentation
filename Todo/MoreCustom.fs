4c761e14bbaf803767c6f648bdf0e9cfd7655e7e
Robin Munn <robin_munn@sil.org>
2016-02-12 11:24:24 +0700

    More playing around with custom operations
    This one was interesting. Using let! resulted in a Bind call with
    some rather interesting implications.

--- Todo/MoreCustom.fs
+++ Todo/MoreCustom.fs
@@ -10,9 +10,15 @@
 
     member x.Yield(()) = 0
 
+    member x.Yield(v) = v
+
+    member x.Bind(v, f) = f
+
     member x.Combine(a, b) = b
 
     member x.Delay(v) = v
+
+    member x.Run(v) = v
 
 let quote = new QuoteBuilder()
 
@@ -26,6 +32,27 @@ Call (Some (ValueWithName (FSI_0155+QuoteBuilder, builder@)), Foo,
 
 *)
 
+(* Note that below, I had to specify the kind of f that Bind() expects, because F# couldn't
+figure it out (it was generic). So there was a value restriction error until I added the
+type signature on "let result":
+
+let result : Quotations.Expr<(string -> int)> = quote {
+    let! x = 5
+    foo 6
+} returns:
+Call (Some (ValueWithName (FSI_0164+QuoteBuilder, builder@)), Delay,
+      [Lambda (unitVar,
+               Call (Some (ValueWithName (FSI_0164+QuoteBuilder, builder@)), Foo,
+                     [Call (Some (ValueWithName (FSI_0164+QuoteBuilder, builder@)),
+                            Bind,
+                            [Value (5),
+                             Lambda (_arg1,
+                                     Let (x, _arg1,
+                                          Call (Some (ValueWithName (FSI_0164+QuoteBuilder,
+                                                                     builder@)),
+                                                Yield, [x])))]), Value (6)]))])
+
+*)
 
 (* quote {
     return! 5
