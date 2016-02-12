3febca26705602960d51db6b59a01299e65ec1d8
Robin Munn <robin_munn@sil.org>
2016-02-12 10:10:07 +0700

    More playing around with custom operations
    This time I'm using builder.Quote() to see the precise sequence of
    calls made by the computation expression. It's sometimes unexpected.

---
+++ Todo/MoreCustom.fs
@@ -0,0 +1,35 @@
+﻿type QuoteBuilder() =
+    member x.Quote(q) = q
+
+    member x.Return v = v
+    
+    member x.ReturnFrom v = v
+
+    [<CustomOperation("foo")>]
+    member x.Foo(a, b) = b
+
+    member x.Yield(()) = 0
+
+    member x.Combine(a, b) = b
+
+    member x.Delay(v) = v
+
+let quote = new QuoteBuilder()
+
+(* quote {
+    foo 6
+} returns:
+
+Call (Some (ValueWithName (FSI_0155+QuoteBuilder, builder@)), Foo,
+      [Call (Some (ValueWithName (FSI_0155+QuoteBuilder, builder@)), Yield,
+             [Value (<null>)]), Value (6)])
+
+*)
+
+
+(* quote {
+    return! 5
+    foo 6
+} wanted a Combine method. Then it wanted a Delay method. Then it said "can't use custom
+expressions with try/with, try/finally, if/then/else, match, or use." Don't know why.
+*)
